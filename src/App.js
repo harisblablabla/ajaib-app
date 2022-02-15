41e8da18813562f558f8fa7fd4231a8dcacd267c
haris fadhilah <haris.fadhilah21@gmail.com>
2022-02-15 19:04:11 +0700

    feat(init): setup bootstrap
    add bootstrap with mdbootstrap for styling, + react router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Suspense } from 'react'
+import { BrowserRouter } from 'react-router-dom';
+const Main = React.lazy(() => import('./module/main'))
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Suspense fallback={<h3>Loading....</h3>}>
+          <div className="App">
+            <Main/>
+          </div>
+        </Suspense>
+    </BrowserRouter>
   );
 }
 
